d3ea4e7dbe3bc0f5312df2e3df0dc81e8720083a
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-26 22:21:29 -0400

    Added react-admin-test application to repo

---
+++ 01_intro/react-admin-test/src/App.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
+import authProvider from './authProvider';
+// import dataProvider from './dataProvider';
+import Dashboard from './Dashboard';
+import { PostCreate, PostEdit, PostList } from "./Posts";
+import { UserList } from "./Users";
+
+// Setup data provider
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+// Create app
+const App = () => (
+  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
+    <Resource name="posts" create={PostCreate} edit={PostEdit} list={PostList} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
+  </Admin>
+);
+
+export default App;
