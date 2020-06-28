1908d86b4a8723362f402d4cb094c9ccd76524d8
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-28 16:46:06 -0400

    Created initial app for 02-data-providers/prisma

---
+++ 02-data-providers/prisma/app/src/Posts.js
@@ -0,0 +1,81 @@
+import * as React from "react";
+import {
+  Create,
+  Datagrid,
+  Edit,
+  EditButton,
+  Filter,
+  List,
+  ReferenceInput,
+  ReferenceField,
+  SelectInput,
+  SimpleForm,
+  SimpleList,
+  TextField,
+  TextInput,
+} from "react-admin";
+import { useMediaQuery } from "@material-ui/core";
+
+const PostTitle = ({ record }) => {
+  return <span>Post {record ? `"${record.title}"` : ""}</span>;
+};
+
+const PostFilter = (props) => (
+  <Filter {...props}>
+    <TextInput label="Search" source="q" alwaysOn />
+    <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+      <SelectInput optionText="name" />
+    </ReferenceInput>
+  </Filter>
+);
+
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
+);
+
+export const PostEdit = (props) => (
+  <Edit title={<PostTitle />} {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostList = (props) => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  return (
+    <List filters={<PostFilter />} {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.title}
+          secondaryText={(record) => `${record.views} views`}
+          tertiaryText={(record) =>
+            new Date(record.published_at).toLocaleDateString()
+          }
+        />
+      ) : (
+        <Datagrid>
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
