25f462ea7735b74c2eb0fa8b738ae2b43b2e3b90
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-29 10:58:00 -0400

    Added query to get all users

--- 02-data-providers/prisma/api/index.ts
+++ 02-data-providers/prisma/api/index.ts
@@ -3,7 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // ... you will write your Prisma Client queries here
+  // Get all users
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
 }
 main()
   .catch((e) => {
